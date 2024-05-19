ad545c48da9ac3097cb0ba09833c35f244aa32dc
Leone Pizzoli <leone.cpizzoli@gmail.com>
2024-05-18 23:07:27 -0300

    feature: Cria aplicação SpringBoot OAuht2 SSO com AWS Cognito

---
+++ src/test/java/br/com/cognitosb/CognitosbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cognitosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitosbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
