8a65894985e018de07b63e8d653725c9b1f4c80b
Leone Pizzoli <leone.cpizzoli@gmail.com>
2024-05-20 00:53:56 -0300

    feature: Altera home page para mostrar nome de usuário que está logado.

---
+++ spring-boot-app/src/main/java/br/com/cognitosb/config/Configuration.java
@@ -0,0 +1,25 @@
+package br.com.cognitosb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@org.springframework.context.annotation.Configuration
+@EnableWebSecurity
+public class Configuration {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+			http
+			 	.authorizeHttpRequests(authorize -> authorize
+					.anyRequest().authenticated()
+				)
+				.oauth2Login(withDefaults());
+			return http.build();
+		}
+	}
+
+
